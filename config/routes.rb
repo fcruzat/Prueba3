fefd8c319a36f67fd00c04581da204d7c7e95113
Francisca Cruzat <franciscacruzat23@gmail.com>
2019-06-04 11:21:09 -0400

    Parte 2, Create creado

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
+  post 'todos', to: 'todos#create'
   get 'todos/new'
   root 'todos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
